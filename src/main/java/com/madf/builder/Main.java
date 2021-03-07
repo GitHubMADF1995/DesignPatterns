658769adcbf62e3ae6251353dbf8f9bb0767ad97
GitHubMADF1995 <1215514252@qq.com>
2021-03-07 15:28:50 +0800

    构建器模式Builder

---
+++ src/main/java/com/madf/builder/Main.java
@@ -0,0 +1,15 @@
+package com.madf.builder;
+
+public class Main {
+    public static void main(String[] args) {
+        TerrainBuilder builder = new ComplexTerrainBuilder();
+        Terrain t = builder.buildFort().buildMine().buildWall().build();
+
+        Person p = new Person.PersonBuilder()
+                .basicInfo(1, "zhangsan", 18)
+                //.score(20)
+                .weight(200)
+                //.loc("bj", "23")
+                .build();
+    }
+}
